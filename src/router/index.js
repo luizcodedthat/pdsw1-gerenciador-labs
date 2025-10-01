2477058166cacb0bc85b4146d77bf8ddd63097a1
Lucas Lins <lucasglins13@gmail.com>
2025-10-01 14:33:15 -0300

    Adicionar rota para página de login

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 import LaboratoriosView from '@/views/LaboratoriosView.vue'
+import LoginView from '@/views/LoginView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -9,8 +10,17 @@ const router = createRouter({
       path: '/laboratorios',
       name: 'Laboratorios',
       component: LaboratoriosView
+    },
+    {
+      path: '/login',
+      name: 'Login',
+      component: LoginView
+    },
+    {
+      path: '/',
+      redirect: '/login' // redireciona a raiz para /login
     }
-  ],
+  ]
 })
 
 export default router
