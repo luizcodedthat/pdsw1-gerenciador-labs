9657b9024dd8335207da66a09575e00ca4725dd5
Luiz Eduardo <57048776+luizcodedthat@users.noreply.github.com>
2025-10-01 01:56:09 -0300

    refactor(css): trocar componentes vue por css global

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import './assets/styles/main.css'
+import './assets/styles/ui.css'
 
 const app = createApp(App)
 
