a076a5386b40250988f79d02bf3c9477d0611ef5
Luiz Eduardo <57048776+luizcodedthat@users.noreply.github.com>
2025-10-04 13:53:54 -0300

    feat(routes): adicionar rota de laboratorio individual

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
+import LaboratorioView from '@/views/LaboratorioView.vue'
 import LaboratoriosView from '@/views/LaboratoriosView.vue'
+
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -9,6 +11,12 @@ const router = createRouter({
       path: '/laboratorios',
       name: 'Laboratorios',
       component: LaboratoriosView
+    },
+    {
+      path: '/laboratorios/:id',
+      name: 'Laboratorio',
+      componente: LaboratorioView,
+      props: true
     }
   ],
 })
