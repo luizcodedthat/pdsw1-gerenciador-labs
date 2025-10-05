716921842d3443ec42ffc35996b827a1d6e9999f
Lucas Lins <lucasglins13@gmail.com>
2025-10-05 11:26:08 -0300

    feat(router): Criar rota de chamados

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 import LaboratoriosView from '@/views/LaboratoriosView.vue'
+import LoginView from '@/views/LoginView.vue'
+import ChamadoView from '@/views/ChamadoView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -9,8 +11,22 @@ const router = createRouter({
       path: '/laboratorios',
       name: 'Laboratorios',
       component: LaboratoriosView
+    },
+    {
+      path: '/login',
+      name: 'Login',
+      component: LoginView
+    },
+    {
+      path: '/',
+      redirect: '/login'
+    },
+    {
+      path: '/chamados',
+      name: 'Chamados',
+      component: ChamadoView
     }
-  ],
+  ]
 })
 
 export default router
