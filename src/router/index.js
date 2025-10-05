653a9eb1f41e712870727ec831644482edc233a7
Luiz Eduardo <57048776+luizcodedthat@users.noreply.github.com>
2025-10-05 00:45:03 -0300

    fix(router): corrigir erro de sintaxe

--- src/router/index.js
+++ src/router/index.js
@@ -25,7 +25,7 @@ const router = createRouter({
     {
       path: '/laboratorios/:id',
       name: 'Laboratorio',
-      componente: LaboratorioView,
+      component: LaboratorioView,
       props: true
     }
   ]
