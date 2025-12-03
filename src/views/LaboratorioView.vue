648080b336f8bf9d7116d8a517095adc08e78769
Luiz Eduardo <57048776+luizcodedthat@users.noreply.github.com>
2025-12-03 13:36:16 -0300

    chore(laboratorio): remover espacos

--- src/views/LaboratorioView.vue
+++ src/views/LaboratorioView.vue
@@ -24,7 +24,7 @@ onMounted(async () => {
 })
 
 function criarChamado(data) {
-  console.log("Chamado criado:", data)
+    console.log("Chamado criado:", data)
 }
 </script>
 
@@ -45,16 +45,9 @@ function criarChamado(data) {
                 </div>
 
                 <!-- ÍCONE ABRE O MODAL -->
-                <PlusCircle
-                    size="24"
-                    @click="modalAberto = true"
-                    style="cursor:pointer;"
-                />
+                <PlusCircle size="24" @click="modalAberto = true" style="cursor:pointer;" />
             </div>
-
-            <div class="card-list">
-                <TicketCardList />
-            </div>
+            <TicketCardList />
         </section>
 
         <section id="comments">
@@ -67,17 +60,20 @@ function criarChamado(data) {
         </section>
 
         <!-- MODAL CHAMADO -->
-        <CreateModal
-            v-model="modalAberto"
-            @save="criarChamado"
-            :labId="id"
-        />
+        <CreateModal v-model="modalAberto" @save="criarChamado" :labId="id" />
 
     </div>
 
 </template>
 
 <style scoped>
+.loading {
+    margin: 10% auto 0;
+    display: block;
+    width: fit-content;
+    font-size: var(--font-size-l);
+}
+
 .wrapper {
     max-width: 1200px;
     margin: 2.5rem auto;
